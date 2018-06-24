a5f02f89c0eb88f02b626f456261647ad0733851
Your Name <you@example.com>
2018-06-24 23:30:10 +0800

    faster

--- demo/unwrap/models.py
+++ demo/unwrap/models.py
@@ -10,26 +10,27 @@ import tensorflow as tf
 import keras
 from keras.models import load_model
 
+keras.backend.clear_session()
+tfconfig = tf.ConfigProto()
+tfconfig.gpu_options.allow_growth = True
+tfsession = tf.Session(config=tfconfig)
+keras.backend.set_session(tfsession)
+model = load_model('./model.h5', custom_objects={'bce_dice_coef':keras.losses.binary_crossentropy, 'focal_loss': keras.losses.binary_crossentropy, 'mean_iou': keras.losses.binary_crossentropy})
+# model.compile(loss='mse', optimizer='sgd')
+graph = tf.get_default_graph()
+input_h, input_w = model.input_shape[1:3]
+output_h, output_w = model.output_shape[1:3]
+
 class IMG(models.Model):
     img = models.ImageField()
     def __init__(self, *args, **kwargs):
         super(IMG, self).__init__(*args, **kwargs)
-        keras.backend.clear_session()
-        self.tfconfig = tf.ConfigProto()
-        self.tfconfig.gpu_options.allow_growth = True
-        self.tfsession = tf.Session(config=self.tfconfig)
-        keras.backend.set_session(self.tfsession)
-        self.model = load_model('./model.h5', custom_objects={'bce_dice_coef':keras.losses.binary_crossentropy, 'focal_loss': keras.losses.binary_crossentropy, 'mean_iou': keras.losses.binary_crossentropy})
-        self.model.compile(loss='mse', optimizer='sgd')
-        self.input_h, self.input_w = self.model.input_shape[1:3]
-        self.output_h, self.output_w = self.model.output_shape[1:3]
         self.original_filename = os.path.split(self.img.path)[-1]
         self.download_filename = os.path.splitext(self.original_filename)[0] + '_wrapped.jpg'
     
     def save(self):
         super(IMG, self).save() # if has same filename. change to different filename
         self.path = self.img.path
-        print(self.img.path)
         t = os.path.splitext(self.img.path)
         self.wrap_path = t[0] + '_wrapped.jpg'
         self.url_path = self.img.url
@@ -38,13 +39,14 @@ class IMG(models.Model):
         raw_image = Image.open(self.img)
         raw_image = raw_image.convert('RGB')
         raw_image = np.array(raw_image, dtype=np.uint8)
-        img_r = np.clip(cv2.resize(raw_image, (self.input_w, self.input_h), interpolation=cv2.INTER_AREA).astype(np.float32)[np.newaxis,...] / 255.0, 0, 1)
+        img_r = np.clip(cv2.resize(raw_image, (input_w, input_h), interpolation=cv2.INTER_AREA).astype(np.float32)[np.newaxis,...] / 255.0, 0, 1)
         h, w = raw_image.shape[:2]
-        label = self.model.predict(img_r, batch_size=1)[0]
+        with graph.as_default():
+            label = model.predict(img_r, batch_size=1)[0]
         pts = find_corner_condidate((label>0.5).astype(np.uint8), 10) # format: (y, x)
         if len(pts)>0:
-            pts[...,0] = np.clip(pts[...,0] * h / self.output_h, 0, h-1)
-            pts[...,1] = np.clip(pts[...,1] * w / self.output_w, 0, w-1)
+            pts[...,0] = np.clip(pts[...,0] * h / output_h, 0, h-1)
+            pts[...,1] = np.clip(pts[...,1] * w / output_w, 0, w-1)
             raw_image = robust_unwarp(raw_image, pts)
         result = Image.fromarray(raw_image)
         result.save(self.wrap_path)
--- demo/unwrap/views.py
+++ demo/unwrap/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render
 from unwrap.models import IMG
+from django.db import transaction
 from django.http import StreamingHttpResponse  
 def upload(request):
     return render(request, 'unwrap/upload.html')
 def mobile(request):
     return render(request, 'unwrap/mobile.html')
 def show(request):
-    new_img = IMG(img=request.FILES.get('img'))
-    new_img.save()
-    content = {
-        'aaa': new_img.url_path,
-        'bbb': new_img.wrap_url_path,
-        'aaa_name': new_img.original_filename,
-        'bbb_name': new_img.download_filename
-    }
+    try:
+        with transaction.atomic():
+            new_img = IMG(img=request.FILES.get('img'))
+            new_img.save()
+            content = {
+                'aaa': new_img.url_path,
+                'bbb': new_img.wrap_url_path,
+                'aaa_name': new_img.original_filename,
+                'bbb_name': new_img.download_filename
+            }
+    except:
+        return render(request, 'unwrap/error.html')
     return render(request, 'unwrap/show.html', content)
 def download(request):
     try:
-        new_img = IMG(img=request.FILES.get('img'))
-        new_img.save()
-        response = StreamingHttpResponse(readFile(new_img.wrap_path))  
-        response['Content-Type']='application/octet-stream'  
-        response['Content-Disposition']='attachment;filename="%s"'%new_img.download_filename
-        return response
+        with transaction.atomic():
+            new_img = IMG(img=request.FILES.get('img'))
+            new_img.save()
+            response = StreamingHttpResponse(readFile(new_img.wrap_path))  
+            response['Content-Type']='application/octet-stream'  
+            response['Content-Disposition']='attachment;filename="%s"'%new_img.download_filename
     except:
-        return render(request, 'unwrap/404.html')
+        return render(request, 'unwrap/error.html')
+    return response
 
 def readFile(filename,chunk_size=512):  
     with open(filename,'rb') as f:  
